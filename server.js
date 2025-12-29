c3095c33be4e728d4aac1db273b84e6d0a447d59
unknown <hannah.nohl@online.de>
2025-12-29 16:20:06 +0100

    change folder

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app = express();
 const PORT = 3000;
 
 // Serve static files from public_html directory
-app.use(express.static(path.join(__dirname, 'public_html')));
+app.use(express.static(path.join(__dirname, 'docs')));
 
 // Serve index.html as the main page
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public_html', 'index.html'));
+  res.sendFile(path.join(__dirname, 'docs', 'index.html'));
 });
 
 app.listen(PORT, () => {
